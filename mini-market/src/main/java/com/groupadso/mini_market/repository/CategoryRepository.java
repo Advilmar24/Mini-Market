0175142a6212db0ef0b29d0c3c5588d215012145
Advilmar24 <alonsomartin2409@gmail.com>
2026-03-22 19:55:38 -0500

    Revert "Feature products"

--- mini-market/src/main/java/com/groupadso/mini_market/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.groupadso.mini_market.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.groupadso.mini_market.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
