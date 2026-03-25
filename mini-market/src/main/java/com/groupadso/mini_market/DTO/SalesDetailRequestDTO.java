c080211826dc0ed3d7b264f4ca119457b624d019
DiegoA-01 <pinedamontenegrodiegoalejandro@gmail.com>
2026-03-24 21:29:33 -0500

    Ultimo commit para que los muchachos ya puedan trabajar

--- mini-market/src/main/java/com/groupadso/mini_market/DTO/SalesDetailRequestDTO.java
+++ mini-market/src/main/java/com/groupadso/mini_market/DTO/SalesDetailRequestDTO.java
@@ -1,12 +1,17 @@
 package com.groupadso.mini_market.DTO;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class SalesDetailRequestDTO {
     // ID del producto que se desea vender
+    @NotNull(message = "El idProduct es obligatorio")
     private Long idProduct;
     
     // Cantidad del producto que se va a vender
+    @NotNull(message = "La cantidad es obligatoria")
+    @Min(value = 1, message = "La cantidad debe ser mayor a 0")
     private Integer cantidad;
 }
